a9cfe6bdabd768016de4e5143a0b1c67807e16a1
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-10-21 18:27:19 +0530

    Changed env namess

--- services/s3Services.js
+++ services/s3Services.js
@@ -1,15 +1,23 @@
-import { DeleteObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  DeleteObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_S3_REGION_CUSTOM || process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY_ID || process.env.AWS_SECRET_KEY,
+    accessKeyId:
+      process.env.AWS_ACCESS_KEY_ID_CUSTOM ||
+      process.env.AWS_ACCESS_KEY,
+    secretAccessKey:
+      process.env.AWS_SECRET_ACCESS_KEY_CUSTOM ||
+      process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
-const bucket = process.env.AWS_BUCKET_NAME;
+const bucket = process.env.AWS_S3_BUCKET_CUSTOM || process.env.AWS_BUCKET_NAME;
 
 export const generatePreSignedUploadURL = async ({ Key, ContentType }) => {
   const command = new PutObjectCommand({
